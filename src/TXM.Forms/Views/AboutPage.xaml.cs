e46d2c56c0868653af58e743d990c3c1bea3a486
Sharpdeveloper <sharpdeveloper@sumhs.de>
2017-12-26 20:34:34 +0100

    Forms init

---
+++ src/TXM.Forms/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TXM.Forms
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
