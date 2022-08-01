f317094952a78ebb59e8fcf04f8626750d2c7f8b
Sharpdeveloper <5103463+Sharpdeveloper@users.noreply.github.com>
2022-08-01 21:34:52 +0200

    maui

---
+++ src/TXM/TXM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TXM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/TXM/TXM/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TXM;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
