9d04ed345490f916b7dd118bf4d8422cabc4444b
Sharpdeveloper <sharpdeveloper@piratesoftatooine.de>
2022-08-01 22:41:15 +0200

    maui 3

---
+++ src/TXM/TXM.Core/Setting.cs
@@ -0,0 +1,46 @@
+﻿namespace TXM.Core
+{
+    public sealed class Settings
+    {
+        #region Constants
+        private const string fileextension = "txmtournament";
+        private const string fileextensionName = "TXM Tournaments";
+        private const string txmversion = "V3.0.0 alpha 1";
+        private const string copyrightYear = "2014 - 2022";
+        #endregion
+
+        #region Properties
+        public static string FILEEXTENSION
+        {
+            get
+            {
+                return fileextension;
+            }
+        }
+
+        public static string TXMVERSION
+        {
+            get
+            {
+                return txmversion;
+            }
+        }
+
+        public static string FILEEXTENSIONSNAME
+        {
+            get
+            {
+                return fileextensionName;
+            }
+        }
+
+        public static string COPYRIGHTYEAR
+        {
+            get
+            {
+                return copyrightYear;
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/TXM/TXM/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace TXM;
+
+using TXM.Core;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		labelVersion.Text = Settings.TXMVERSION;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+
